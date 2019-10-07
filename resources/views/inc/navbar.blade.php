b5f6f572d95660ae76b2ccc11f22ba4d3f2be43e
kei1987 <kei.usami1987@gmail.com>
2019-10-07 23:45:07 +0900

    initial commit

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -14,6 +14,14 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/services" tabindex="-1">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/posts" tabindex="-1">Blog</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav nav navbar-right">
+        <li>
+          <a href="/posts/create">Create post</a>
         </li>
       </ul>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,23 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
+        <script src='https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js' referrerpolicy="origin"></script>
+        <script>
+        tinymce.init({
+          selector: '#editor'
+        });
+        </script>
+    
     </head>
     <body>
       @include('inc.navbar')
+      @include('inc.messages')
       <div class="container">
         @yield('content')
       </div>
+      <script src={{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}></script>
+      <script>
+          CKEDITOR.replace( 'article-ckeditor' );
+      </script>
     </body>
 </html>
